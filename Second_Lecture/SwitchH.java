d6a6c6b780da9771b4a92769ff6975b9077c8eea
MohammedAnasuddinZaid <mohammedanasuddinzaid256@gmail.com>
2026-07-29 08:39:47 +0530

    2nd lecture done fuly

---
+++ Second_Lecture/SwitchH.java
@@ -0,0 +1,33 @@
+
+import java.util.Scanner;
+
+
+
+
+public class SwitchH {
+    
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int button = sc.nextInt();
+
+        switch(button){
+            case 1: System.out.println("HELLO");
+            break;
+            case 2: System.out.println("NAMASTE");
+            break;
+            case 3: System.out.println("BONJOUR");
+            break;
+            default: System.out.println("INVALID BUTTION");
+            break;
+
+        }
+
+
+    }
+
+        
+
+    
+}
---
+++ Second_Lecture/ifelse.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class ifelse {
+    public static void main(String args[]) {
+        
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>17){
+            System.out.println("YES YOU CAN VOTE");
+        }else{
+            System.out.println("NO YOU ARE NOT A ADULT");
+        }
+
+
+
+
+    }
+    
+}
